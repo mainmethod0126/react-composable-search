18db845095dd004688d02dd5f41ce893dd511e21
wusub <mainmethod0126@gmail.com>
2025-10-12 15:22:34 +0900

    select ui 설계중, 함수형 방식으로 바꿔야하나? 클래스를 사용하는 방식이 레거시하다고 들었는데 이걸 유지할지 검토가 필요할듯

--- src/components/Select/RegionSelect/RegionSelect.tsx
+++ src/components/Select/RegionSelect/RegionSelect.tsx
@@ -9,10 +9,29 @@ export class RegionSelect implements ComposableSelect {
 
     placeHolder?: string;
 
+    isOpen: boolean;
+
     constructor(options: { placeHolder?: string }) {
         this.placeHolder = options?.placeHolder;
     }
 
+
+    LocationMarkerIcon = () => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z" />
+            <circle cx="12" cy="9" r="3" />
+        </svg>
+    );
 
     onChange = (selectedItems: ComposableSelectItem[]) => {
         console.log(selectedItems)
@@ -23,10 +42,13 @@ export class RegionSelect implements ComposableSelect {
         return (
             <div className="composable-select-container">
                 <div className="composable-select-head-icon">
-                    icon
+                    <this.LocationMarkerIcon />
                 </div>
                 <div className="composable-select-trigger">
                     region select
+                    <div>
+                        화살표
+                    </div>
                 </div>
             </div>
         )
